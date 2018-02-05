07c33800a39b1876bd9506aca67bba423d0b0a0e
Jeremy Warner <jeremywrnr@gmail.com>
2018-02-05 10:07:47 -0500

    add setAttributeNS

--- beadweaver.js
+++ beadweaver.js
@@ -1,11 +1,16 @@
 window.onload = function() {
     console.log("hello world")
+    var xmlns = "http://www.w3.org/2000/svg"
+    var xlink = "http://www.w3.org/1999/xlink"
     var embedSvg = document.getElementById("patternSvg").contentDocument
     var svgNode = embedSvg.getElementById("svg")
-    var beadNode = document.createElementNS("http://www.w3.org/2000/svg", "use")
-    beadNode.setAttribute("xlink:href", "#singleBead")
+    var beadNode = document.createElementNS(xmlns, "use")
+    beadNode.setAttributeNS(xlink, "xlink:href", "#singleBead")
     generateBeadGrid(svgNode, beadNode, 3, 3)
 }
+
+// from this ref:
+// http://metaskills.net/2014/08/29/dynamic-svgs-using-defs-elements-and-javascript/
 
 function setSvgDimensions(svg, width, height, unit) {
     // set svg dimensions
@@ -22,8 +27,8 @@ function generateBeadGrid(svg, beadNode, rows, columns) {
     var screenUnit = "mm"
     var svgWidth = beadWidth * columns + xPadding * (columns - 1)
     var svgHeight = beadHeight * rows + yPadding * (rows - 1)
-    console.log(svgWidth)
-    setSvgDimensions(svg, svgWidth, svgHeight, "mm")
+    console.log(svgWidth + screenUnit)
+    setSvgDimensions(svg, svgWidth, svgHeight, screenUnit)
     for (j = 0; j < columns; j++) {
         var xPos = beadWidth * j + xPadding * j
         for (i = 0; i < rows; i++) {
