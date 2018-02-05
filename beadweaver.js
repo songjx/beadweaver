77be7c3629177bee3bc9c1dcd323b1e4d31b0c7e
Jackie Song <songjx@users.noreply.github.com>
2018-02-04 21:38:40 -0800

    generate grid with offset
    use setAttributeNS() for xlink:href

--- beadweaver.js
+++ beadweaver.js
@@ -3,12 +3,11 @@ window.onload = function() {
     var embedSvg = document.getElementById("patternSvg").contentDocument
     var svgNode = embedSvg.getElementById("svg")
     var beadNode = document.createElementNS("http://www.w3.org/2000/svg", "use")
-    beadNode.setAttribute("xlink:href", "#singleBead")
-    generateBeadGrid(svgNode, beadNode, 3, 3)
+    beadNode.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', "#singleBead");
+    generateBeadGrid(svgNode, beadNode, 20, 8)
 }
 
 function setSvgDimensions(svg, width, height, unit) {
-    // set svg dimensions
     svg.setAttribute("width", String(width) + unit)
     svg.setAttribute("height", String(height) + unit)
     svg.setAttribute("viewBox", "0 0 " + String(width) + " " + String(height))
@@ -17,26 +16,20 @@ function setSvgDimensions(svg, width, height, unit) {
 function generateBeadGrid(svg, beadNode, rows, columns) {
     var beadWidth = 13
     var beadHeight = 16
-    var xPadding = 1
-    var yPadding = 1
-    var screenUnit = "mm"
-    var svgWidth = beadWidth * columns + xPadding * (columns - 1)
-    var svgHeight = beadHeight * rows + yPadding * (rows - 1)
-    console.log(svgWidth)
-    setSvgDimensions(svg, svgWidth, svgHeight, "mm")
+    var xPadding = 0
+    var yPadding = .3
+    var screenUnit = "pt"
+    var svgWidth = beadWidth*columns + xPadding*(columns - 1)
+    var svgHeight = beadHeight*(rows + 0.5) + yPadding*(rows - 1 + 0.5)
+    setSvgDimensions(svg, svgWidth, svgHeight, screenUnit)
     for (j = 0; j < columns; j++) {
         var xPos = beadWidth * j + xPadding * j
         for (i = 0; i < rows; i++) {
-            var yPos = beadHeight * i + yPadding * i
+            var yPos = beadHeight*i + yPadding*i + (j%2)/2 * (beadHeight + yPadding/2)
             var newBeadNode = beadNode.cloneNode()
             newBeadNode.setAttribute("x", xPos)
             newBeadNode.setAttribute("y", yPos)
             svg.appendChild(newBeadNode)
-            console.log(
-                String(j) + ' ' +
-                String(i) + ' ' +
-                String(xPos) + ' ' +
-                String(yPos))
         }
     }
 }
