aaf3c8c1fa820d80953a9082def84fa395e27f97
Jackie Song <songjx@users.noreply.github.com>
2018-02-06 22:26:08 -0800

    reuse path data
    also name and consolidate gradients

--- beadweaver.js
+++ beadweaver.js
@@ -3,7 +3,7 @@ window.onload = function() {
     var embedSvg = document.getElementById("patternSvg").contentDocument
     var svgNode = embedSvg.getElementById("svg")
     var beadNode = document.createElementNS("http://www.w3.org/2000/svg", "use")
-    beadNode.setAttribute("href", "#singleBead");
+    beadNode.setAttribute("href", "#singleBead")
     generateBeadGrid(svgNode, beadNode, 20, 8)
 }
 
@@ -19,13 +19,15 @@ function generateBeadGrid(svg, beadNode, rows, columns) {
     var xPadding = -1
     var yPadding = 0
     var screenUnit = "pt"
+    var strokeWidth = 0.6
     var svgWidth = beadWidth*columns + xPadding*(columns - 1)
-    var svgHeight = beadHeight*(rows + 0.5) + yPadding*(rows - 1 + 0.5)
+    var svgHeight = beadHeight*(rows + 0.5) + yPadding*(rows - 1 + 0.5) + strokeWidth
     setSvgDimensions(svg, svgWidth, svgHeight, screenUnit)
     for (j = 0; j < columns; j++) {
         var xPos = beadWidth * j + xPadding * j
         for (i = 0; i < rows; i++) {
-            var yPos = beadHeight*i + yPadding*i + (j%2)/2 * (beadHeight + yPadding/2)
+            // ugly...
+            var yPos = strokeWidth/2 + beadHeight*i + yPadding*i + (j%2)/2 * (beadHeight + yPadding/2)
             var newBeadNode = beadNode.cloneNode()
             newBeadNode.setAttribute("x", xPos)
             newBeadNode.setAttribute("y", yPos)
