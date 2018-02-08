74b68642d68f0ed6f538a8bdb4de745b506677df
Jackie Song <songjx@users.noreply.github.com>
2018-02-07 22:04:37 -0800

    beads with <g> instead of <use>
    no more shadow DOM boundary crossing issue with descendant CSS selectors

--- beadweaver.js
+++ beadweaver.js
@@ -2,9 +2,11 @@ window.onload = function() {
     console.log("hello world")
     var embedSvg = document.getElementById("patternSvg").contentDocument
     var svgNode = embedSvg.getElementById("svg")
-    var beadNode = document.createElementNS("http://www.w3.org/2000/svg", "use")
-    beadNode.setAttribute("href", "#singleBead")
-    generateBeadGrid(svgNode, beadNode, 20, 8)
+    var beadNode = svgNode.getElementById("singleBead")
+    beadNode.removeAttribute("id")
+    var beadArray = generateBeadGrid(svgNode, beadNode, 20, 8)
+    beadArray[3][3].setAttribute("class", "bead-3")
+    setBeadRange(5, undefined, 5, 7, beadArray, "bead-3")
 }
 
 function setSvgDimensions(svg, width, height, unit) {
@@ -23,14 +25,15 @@ function generateBeadGrid(svg, beadNode, rows, columns) {
     var svgWidth = beadWidth*columns + xPadding*(columns - 1)
     var svgHeight = beadHeight*(rows + 0.5) + yPadding*(rows - 1 + 0.5) + strokeWidth
     setSvgDimensions(svg, svgWidth, svgHeight, screenUnit)
-    for (j = 0; j < columns; j++) {
-        var xPos = beadWidth * j + xPadding * j
-        for (i = 0; i < rows; i++) {
+    var beadArray = []
+    for (i = 0; i < rows; i++) {
+        var row = []
+        for (j = 0; j < columns; j++) {
+            var xPos = beadWidth * j + xPadding * j
             // ugly...
             var yPos = strokeWidth/2 + beadHeight*i + yPadding*i + (j%2)/2 * (beadHeight + yPadding/2)
-            var newBeadNode = beadNode.cloneNode()
-            newBeadNode.setAttribute("x", xPos)
-            newBeadNode.setAttribute("y", yPos)
+            var newBeadNode = beadNode.cloneNode(true)
+            newBeadNode.setAttribute("transform", "translate(" + String(xPos) + " " + String(yPos) + ")")
             if (i%2) {
                 newBeadNode.setAttribute("class", "bead-1")
             }
@@ -38,6 +41,21 @@ function generateBeadGrid(svg, beadNode, rows, columns) {
                 newBeadNode.setAttribute("class", "bead-2")
             }
             svg.appendChild(newBeadNode)
+            row.push(newBeadNode)
+        }
+        beadArray.push(row)
+    }
+    return beadArray
+}
+
+function setBeadRange(row1, row2, column1, column2, beadArray, setClass) {
+    // slice beads of interest into beadRange
+    var beadRowRange = beadArray.slice(row1, row2)
+    for (j = 0; j < beadRowRange.length; j++) {
+        rowSlice = beadRowRange[j].slice(column1, column2)
+        for (i = 0; i < rowSlice.length; i++) {
+            rowSlice[i].setAttribute("class", "bead-3")
         }
     }
+
 }
