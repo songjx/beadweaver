3d8a6f0b79eb342256e4acc949b569e81df7eecd
Jackie Song <songjx@users.noreply.github.com>
2018-02-22 22:18:22 -0800

    make Pattern class

--- beadweaver.js
+++ beadweaver.js
@@ -3,51 +3,59 @@ window.onload = function() {
     var embedSvg = document.getElementById("patternSvg").contentDocument
     var svgNode = embedSvg.getElementById("svg")
     var beadNode = svgNode.getElementById("singleBead")
-    var beadClasses = generateBeadClasses()
     beadNode.removeAttribute("id")
-    var beadArray = generateBeadGrid(svgNode, beadNode, 20, 8)
-    beadArray[3][3].setAttribute("class", beadClasses[2])
-    setBeadRange(5, undefined, 5, 7, beadArray, beadClasses[2])
+    var beadingPattern = new Pattern(svgNode, beadNode)
+    var beadArray = beadingPattern.generateBeadGrid(beadNode, 20, 8)
+    beadArray[3][3].setAttribute("class", beadingPattern.beadClasses[2])
+    setBeadRange(5, undefined, 5, 7, beadArray, beadingPattern.beadClasses[2])
 }
 
-function setSvgDimensions(svg, width, height, unit) {
-    svg.setAttribute("width", String(width) + unit)
-    svg.setAttribute("height", String(height) + unit)
-    svg.setAttribute("viewBox", "0 0 " + String(width) + " " + String(height))
-}
-
-function generateBeadGrid(svg, beadNode, rows, columns) {
-    var beadWidth = 13
-    var beadHeight = 16
-    var xPadding = -1
-    var yPadding = 0
-    var screenUnit = "pt"
-    var strokeWidth = 0.6
-    var svgWidth = beadWidth*columns + xPadding*(columns - 1)
-    var svgHeight = beadHeight*(rows + 0.5) + yPadding*(rows - 1 + 0.5) + strokeWidth
-    setSvgDimensions(svg, svgWidth, svgHeight, screenUnit)
-    var beadArray = []
-    for (i = 0; i < rows; i++) {
-        var row = []
-        for (j = 0; j < columns; j++) {
-            var xPos = beadWidth * j + xPadding * j
-            // ugly...
-            var yPos = strokeWidth/2 + beadHeight*i + yPadding*i + (j%2)/2 * (beadHeight + yPadding/2)
-            var newBeadNode = beadNode.cloneNode(true)
-            newBeadNode.setAttribute("transform", "translate(" + String(xPos) + " " + String(yPos) + ")")
-            if (i%2) {
-                newBeadNode.setAttribute("class", "bead-1")
-            }
-            else {
-                newBeadNode.setAttribute("class", "bead-2")
-            }
-            newBeadNode.onclick = beadClick
-            svg.appendChild(newBeadNode)
-            row.push(newBeadNode)
-        }
-        beadArray.push(row)
+function Pattern(svgNode, beadNode) {
+    this.svg = svgNode
+    this.beadNode = beadNode
+    this.beadClasses = ["bead-0", "bead-1", "bead-2"]
+    this.setSvgDimensions = function(width, height, unit) {
+        this.svg.setAttribute("width", String(width) + unit)
+        this.svg.setAttribute("height", String(height) + unit)
+        this.svg.setAttribute("viewBox", "0 0 " + String(width) + " " + String(height))
     }
-    return beadArray
+    this.beadClick = function(beadNode, newClass) {
+        beadNode.setAttribute("class", newClass)
+    }
+    this.generateBeadGrid = function(beadNode, rows, columns) {
+        var beadWidth = 13
+        var beadHeight = 16
+        var xPadding = -1
+        var yPadding = 0
+        var screenUnit = "pt"
+        var strokeWidth = 0.6
+        var svgWidth = beadWidth*columns + xPadding*(columns - 1)
+        var svgHeight = beadHeight*(rows + 0.5) + yPadding*(rows - 1 + 0.5) + strokeWidth
+        this.setSvgDimensions(svgWidth, svgHeight, screenUnit)
+        var beadArray = []
+        for (i = 0; i < rows; i++) {
+            var row = []
+            for (j = 0; j < columns; j++) {
+                var xPos = beadWidth * j + xPadding * j
+                // ugly...
+                var yPos = strokeWidth/2 + beadHeight*i + yPadding*i + (j%2)/2 * (beadHeight + yPadding/2)
+                var newBeadNode = beadNode.cloneNode(true)
+                newBeadNode.setAttribute("transform", "translate(" + String(xPos) + " " + String(yPos) + ")")
+                newBeadNode.setAttribute("class", this.beadClasses[0])
+                var context = this
+                newBeadNode.addEventListener(
+                    "click",
+                    function(event) {
+                        return context.beadClick(event.currentTarget, context.beadClasses[1])
+                    }
+                )
+                this.svg.appendChild(newBeadNode)
+                row.push(newBeadNode)
+            }
+            beadArray.push(row)
+        }
+        return beadArray
+    }
 }
 
 function setBeadRange(row1, row2, column1, column2, beadArray, setClass) {
@@ -56,17 +64,7 @@ function setBeadRange(row1, row2, column1, column2, beadArray, setClass) {
     for (j = 0; j < beadRowRange.length; j++) {
         rowSlice = beadRowRange[j].slice(column1, column2)
         for (i = 0; i < rowSlice.length; i++) {
-            rowSlice[i].setAttribute("class", "bead-3")
+            rowSlice[i].setAttribute("class", setClass)
         }
     }
-
-}
-
-function beadClick(event) {
-    event.currentTarget.setAttribute("class", "bead-1")
-}
-
-function generateBeadClasses() {
-    var beadClasses = ["bead-1", "bead-2", "bead-3"]
-    return beadClasses
 }
